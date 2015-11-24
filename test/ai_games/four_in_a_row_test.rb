7e5793dd4e17fb8df8e3279eb75001f8d0c45c3e
Jan David <jandavid@awesometechnology.de>
2015-11-24 02:10:25 +0100

    Remove default failing test
    By default, a single failing test is included with the gem skeleton.
    Since the module won't do much work in this library, it is not likely
    that the test file will be touched soon. Therefore, the test has been
    removed to make the test suite complete successfully.

--- test/ai_games/four_in_a_row_test.rb
+++ test/ai_games/four_in_a_row_test.rb
@@ -5,9 +5,5 @@ module AIGames
     def test_that_it_has_a_version_number
       refute_nil ::AIGames::FourInARow::VERSION
     end
-
-    def test_it_does_something_useful
-      assert false
-    end
   end
 end
