79721fc4002799d094a99f87335148d9cedd849d
Jan David <jandavid@awesometechnology.de>
2015-11-24 02:19:17 +0100

    Implement playing field
    A class has been added to represent the playing field. It uses a two-
    dimensional array as its underlying data structure, but abstracts from
    that by providing its own implementation of []. When changing the size
    of the playing field, the array is extended when necessary.

---
+++ lib/ai_games/four_in_a_row/playing_field.rb
@@ -0,0 +1,86 @@
+# Copyright (c) 2015 jdno
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+module AIGames
+  module FourInARow
+    # This class represents the playing field of the game. It has a given number
+    # of columns and rows, and each field can either be empty or filled. If a
+    # field is filled, the name of the player whose chip fills the field is
+    # stored in it.
+    class PlayingField
+      # The number of columns of the playing field
+      attr_reader :columns
+
+      # The number of rows of the playing field
+      attr_reader :rows
+
+      # A two-dimensional array containing all the fields of the playing field
+      attr_reader :fields
+
+      # Initialize the playing field with the given number of rows and columns.
+      def initialize(rows = 0, columns = 0)
+        @columns = columns
+        @rows = rows
+        @fields = Array.new(rows) { Array.new(columns) }
+
+        (0...rows).each do |i|
+          (0...columns).each do |j|
+            @fields[i][j] = nil
+          end
+        end
+      end
+
+      # Shortcut to the field elements, using the playing field itself like the
+      # underlying array structure.
+      def [](index)
+        @fields[index]
+      end
+
+      # Sets number of rows. If the new number of rows is greater than the
+      # current number of rows, the array gets resized.
+      def rows=(rows)
+        if rows > @rows
+          (@rows...rows).each do |i|
+            @fields[i] = Array.new(columns)
+
+            (0...columns).each do |j|
+              @fields[i][j] = nil
+            end
+          end
+        end
+
+        @rows = rows
+      end
+
+      # Sets the number of columns. If the new number of columns is greater than
+      # the current number of columns, the array gets resized.
+      def columns=(columns)
+        if columns > @columns
+          (0...rows).each do |i|
+            (@columns...columns).each do
+              @fields[i] << nil
+            end
+          end
+        end
+
+        @columns = columns
+      end
+    end
+  end
+end
