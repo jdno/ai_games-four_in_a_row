d5a57b805e2adcce46b863b58ba6b109ab3e7f29
Jan David <jandavid@awesometechnology.de>
2015-11-27 00:02:09 +0100

    Add cells to playing field
    The class Cell has been added to represents the cells of the playing
    field, containing its position and the possible owner.

---
+++ lib/ai_games/four_in_a_row/cell.rb
@@ -0,0 +1,42 @@
+# Copyright (c) 2015 Jan David Nose
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+module AIGames
+  module FourInARow
+    # The playing field consists of cells. Each cell is located in a certain row
+    # and column, and can belong to one of the players.
+    class Cell
+      # The row the cell is in
+      attr_reader :row
+
+      # The column the cell is in
+      attr_reader :column
+
+      # The bot who fills this cell
+      attr_accessor :owner
+
+      # Initializes the cell with the given position, and optionally the owner.
+      def initialize(row, column, owner = nil)
+        @row = row
+        @column = column
+        @owner = owner
+      end
+    end
+  end
+end
--- test/ai_games/playing_field_test.rb
+++ test/ai_games/playing_field_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
-require 'ai_games/four_in_a_row/playing_field'
 
 module AIGames
   module FourInARow
     class PlayingFieldTest < Minitest::Test
       def setup
         @playing_field = PlayingField.new(2, 4)
+        @fields = @playing_field.instance_variable_get(:@fields)
       end
 
       def test_initialization
@@ -14,21 +14,21 @@ module AIGames
 
         (0...2).each do |i|
           (0...4).each do |j|
-            assert_nil @playing_field.instance_variable_get(:@fields)[i][j]
+            assert_kind_of Cell, @fields[i][j]
           end
         end
       end
 
       def test_get_cell
         bot = Bot.new
-        @playing_field.instance_variable_get(:@fields)[0][0] = bot
+        @fields[0][0] = bot
         assert_equal bot, @playing_field.get_cell(0, 0)
       end
 
       def test_set_cell
         bot = Bot.new
         @playing_field.set_cell(0, 0, bot)
-        assert_equal bot, @playing_field.instance_variable_get(:@fields)[0][0]
+        assert_equal bot, @fields[0][0].owner
       end
 
       def test_rows=
@@ -36,7 +36,7 @@ module AIGames
 
         assert 3, @playing_field.rows
         (0...4).each do |j|
-          assert_nil @playing_field.get_cell(2, j)
+          assert_kind_of Cell, @playing_field.get_cell(2, j)
         end
       end
 
@@ -45,7 +45,7 @@ module AIGames
 
         assert 5, @playing_field.columns
         (0...2).each do |i|
-          assert_nil @playing_field.get_cell(i, 4)
+          assert_kind_of Cell, @playing_field.get_cell(i, 4)
         end
       end
     end
